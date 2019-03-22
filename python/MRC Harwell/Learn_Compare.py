6dfb070153f7dac213f4c7397cb9532d2643b96d
Michael P Camilleri <michael.p.camilleri@ed.ac.uk>
2019-03-22 10:40:07 +0000

    Scripts:  * Added feature to control Pi in Learn_Compare

--- python/MRC Harwell/Learn_Compare.py
+++ python/MRC Harwell/Learn_Compare.py
@@ -23,9 +23,9 @@ DEFAULTS = \
     {'Output': ['../../data/Compare_DS', '../../data/Compare_ISAR'],  # File-Names
      'Random': '0',                                                   # Random Seed offset
      'Numbers': ['0', '20'],                                          # Range: start index, number of runs
-     'Lengths': ['100', '50'],                                       # Number and length of segments
-     'Schemas': ['13', '15', '17', '10'],                             # Probability over Schemas,  #
-     'Uniform': False,                                                 # Set Pi to Uniform
+     'Lengths': ['60', '5400'],                                       # Number and length of segments
+     'Schemas': ['13', '15', '17', '10'],                             # Probability over Schemas,
+     'Pi': 'true',                                                    # Pi mode
      'Folds': '10'}                                                   # Number of Folds to use (folding is by Segment)
 
 # Some Constants
@@ -62,13 +62,15 @@ if __name__ == '__main__':
                             .format(DEFAULTS['Folds']), default=DEFAULTS['Folds'])
     _arg_parse.add_argument('-s', '--schemas', help='Schema Probabilities. Default is: {}'.format(DEFAULTS['Schemas']),
                             default=DEFAULTS['Schemas'], nargs=sS)
-    _arg_parse.add_argument('-u', '--uniform', help='If set, then Pi is a uniform distribution (i.e. all states are '
-                                                    'equally likely.', action='store_true')
+    _arg_parse.add_argument('-p', '--pi', help='How to simulate Pi: can be {{true}} to use the PI learnt from the MRC '
+                                               'Harwell data, {{unif}} to use a fully uniform distribution or {{f}} '
+                                               'where f is a number, in which case it is interpreted as the alpha'
+                                               'weight for the Dirichlet from which Pi is sampled. Default is "{}"'
+                            .format(DEFAULTS['Pi']), default=DEFAULTS['Pi'])
     _arg_parse.add_argument('-a', '--all', help='If Set, then store all the data (i.e. including the simulation): note '
                                                 'that this will generate large files. Default is off.', dest='save_all',
                             action='store_true')
     _arg_parse.set_defaults(save_all=False)
-    _arg_parse.set_defaults(uniform=DEFAULTS['Uniform'])
     # ---- Parse Arguments and transform as required ---- #
     args = _arg_parse.parse_args()
     args.random = int(args.random)
@@ -85,7 +87,12 @@ if __name__ == '__main__':
     # ---- Load Baseline Models ---- #
     with np.load('../../data/model.mrc.npz') as _data:
         omega = _data['omega']
-        pi = _data['pi'] if not args.uniform else np.random.dirichlet(np.full_like(_data['pi'], 20))
+        if args.pi.lower() == 'true':
+            pi = _data['pi']
+        elif args.pi.lower() == 'unif':
+            pi = npext.sum_to_one(np.ones_like(_data['pi']))
+        else:
+            pi = np.random.dirichlet(np.full_like(_data['pi'], float(args.pi)))
         psi = _data['psi']
     # ---- Extract Sizes ---- #
     sZ, sK, sU = psi.shape       # Latent Model [Z, #Annotators, U]
