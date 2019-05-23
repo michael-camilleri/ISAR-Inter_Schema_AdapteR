BUG Fix: ISAR was using wrong prior computation

isar/models/InterSchemaAdapteR.py
@@ -402,9 +402,9 @@ class InterSchemaAdapteRIID(WorkerHandler):
 
                 # -------------- M-Step: -------------- #
                 # Now Compute Pi
-                pi = npext.sum_to_one(np.sum(msg.gamma, axis=0).squeeze() + _common.prior_pi, axis=-1)
+                pi = npext.sum_to_one(np.sum(msg.gamma, axis=0).squeeze() + (_common.prior_pi - 1), axis=-1)
                 # Finally Compute Psi
-                psi = npext.sum_to_one(msg.rho_sum + _common.prior_psi, axis=-1)
+                psi = npext.sum_to_one(msg.rho_sum + (_common.prior_psi - 1), axis=-1)
 
                 # -------------- Iteration Control -------------- #
                 iterations += 1

scripts/Learn_Compare.py
@@ -17,13 +17,13 @@ from isar.models import DawidSkeneIID, InterSchemaAdapteRIID
 
 # Default Parameters
 DEFAULTS = \
-    {'Output': ['../data/Compare_DS', '../data/Compare_ISAR'],  # File-Names
+    {'Output': ['../data/Compare_DS_F1', '../data/Compare_ISAR_F1'],  # File-Names
      'Random': '0',                                             # Random Seed offset
-     'Numbers': ['0', '20'],                                    # Range: start index, number of runs
-     'Lengths': ['500', '100'],                                 # Number and length of segments
+     'Numbers': ['0', '10'],                                    # Range: start index, number of runs
+     'Lengths': ['50', '100'],                                  # Number and length of segments
      'Schemas': ['13', '15', '17', '10'],                       # Probability over Schemas,
      'Pi': 'unif',                                              # Pi mode
-     'Folds': '10'}                                             # Number of Folds to use (folding is by Segment)
+     'Folds': '5'}                                             # Number of Folds to use (folding is by Segment)
 
 # Some Constants
 PDF_ANNOT = npext.sum_to_one([49, 31, 11, 4, 10, 25, 9, 7, 6, 3, 3])  # Probability over Annotators
@@ -96,13 +96,15 @@ if __name__ == '__main__':
     if args.output[DS].lower() != 'none':
         pred_corr_DS = np.zeros([run_length, sS], dtype=float)      # Array for Absolute Accuracy
         pred_wght_DS = np.zeros([run_length, sS], dtype=float)      # Array for Predictive Log Probability
+        pred_f1_DS = np.zeros([run_length, sS+1], dtype=float)      # F1-Score Array - this requires global computation
     else:
-        pred_corr_DS = None; pred_wght_DS = None
+        pred_corr_DS = None; pred_wght_DS = None; pred_f1_DS = None
     if args.output[ISAR].lower() != 'none':
         pred_corr_ISAR = np.zeros([run_length, sS], dtype=float)
         pred_wght_ISAR = np.zeros([run_length, sS], dtype=float)
+        pred_f1_ISAR = np.zeros([run_length, sS+1], dtype=float)
     else:
-        pred_corr_ISAR = None; pred_wght_ISAR = None
+        pred_corr_ISAR = None; pred_wght_ISAR = None; pred_f1_ISAR = None
     run_sizes = np.zeros([run_length, sS], dtype=float)         # Number of Samples per Schema per Run
 
     # --- Iterate over Runs ---- #
@@ -124,6 +126,8 @@ if __name__ == '__main__':
         # [B] - Train ISAR Model
         if args.output[ISAR].lower() != 'none':
             print(' - Training ISAR Model (holistically):')
+            # Prepare some placeholders for the F1-scores
+            z_predictions = np.empty_like(Z)
             for fold in range(sF):  # Iterate over folds
                 print(' ---> Fold {}'.format(fold))
                 # Split Training/Testing Sets

scripts/Visualise_Compare.py
@@ -5,7 +5,7 @@ from scipy.stats import ttest_rel
 import numpy as np
 import argparse
 
-DEFAULTS = {'Results': ['../data/Compare_DS.npz', '../data/Compare_ISAR.npz'],
+DEFAULTS = {'Results': ['../data/Compare_DS_F1.npz', '../data/Compare_ISAR_F1.npz'],
             'Schemas': ['I', 'II', 'III', 'IV']}
 
 if __name__ == '__main__':
@@ -42,7 +42,7 @@ if __name__ == '__main__':
               .format(args.schemas[s], acc_ds_schema[:, s].mean()*100, acc_ds_schema[:, s].std()*100,
                       acc_isar_schema[:, s].mean()*100, acc_isar_schema[:, s].std()*100,
                       *ttest_rel(acc_ds_schema[:, s], acc_isar_schema[:, s])))
-    # [B] - Now do global
+    # [B] - Now do global: here have to take into consideration the number of runs in each schema for a balanced mean!
     acc_ds_global = np.divide(accuracy_ds.sum(axis=1), sizes_all.sum(axis=1))
     acc_isar_global = np.divide(accuracy_isar.sum(axis=1), sizes_all.sum(axis=1))
     print('--------------------------------------------------------------------------')